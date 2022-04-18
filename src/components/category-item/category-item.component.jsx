15239da1c6c36704fe5b9c4ac281121726571a70
vupham <vuquangpham2909@gmail.com>
2022-04-18 22:27:29 +0700

    update directory and category components

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,23 @@
+import "./categories.styles.scss";
+
+import React from "react";
+
+function CategoryItem({ category }) {
+  const { title, imageUrl } = category;
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+}
+
+export default CategoryItem;
